fbcc483fb74a84c6d53b8845c0338e95ae1f0780
Denish099 <gyldenish@gmail.com>
2025-06-17 19:58:37 +0530

    added axiosInstance and added query to dashboard page

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,31 @@
+import { Route, Routes } from "react-router-dom";
+import axiosInstance from "./lib/axios";
 import { Dashboard } from "./pages/dashboard";
 import { SignUp } from "./pages/signup";
+import { useQuery } from "@tanstack/react-query";
+import { Navigate } from "react-router-dom";
+import { Login } from "./pages/signin";
 function App() {
+  const { data: authUser } = useQuery({
+    queryKey: ["authUser"],
+    queryFn: async () => {
+      const res = await axiosInstance.get("/auth/me");
+      return res.data;
+    },
+  });
   return (
     <div>
-      <Dashboard />
-      <SignUp />
+      <Routes>
+        <Route path="/" element={authUser ? <Dashboard /> : <SignUp />} />
+        <Route
+          path="/signup"
+          element={!authUser ? <SignUp /> : <Navigate to={"/"} />}
+        />
+        <Route
+          path="/login"
+          element={!authUser ? <Login /> : <Navigate to="/" />}
+        />
+      </Routes>
     </div>
   );
 }
